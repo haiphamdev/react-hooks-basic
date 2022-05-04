1dbc4c2cccea8753377cddcf6f5f417bf3ae89fe
Hai Pham <haipham.ndev@gmail.com>
2022-05-04 13:08:52 +0700

    Add feature post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
+import PostList from "./components/PostList";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
@@ -10,6 +11,32 @@ function App() {
     { id: 2, title: "We love Easy Frontend! 🥰 " },
     { id: 3, title: "They love Easy Frontend! 🚀 " },
   ]);
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log({ responseJSON });
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log("Failed to fetch post list: ", error.message);
+      }
+    }
+
+    console.log("POST list effect");
+    fetchPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log("TODO list effect");
+  });
 
   const handleTodoClick = (todo) => {
     const index = todoList.findIndex((x) => x.id === todo.id);
@@ -37,8 +64,9 @@ function App() {
   return (
     <div className="app">
       <h2>Welcome to React Hooks!</h2>
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+      <PostList posts={postList} />
     </div>
   );
 }
---
+++ src/components/PostList/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+PostList.propTypes = {
+  posts: PropTypes.array,
+};
+
+function PostList({ posts = [] }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default PostList;
