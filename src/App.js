3be7c03dfa534e437df1dd1d8cbec28625ebdfbc
Hai Pham <haipham.ndev@gmail.com>
2022-05-04 11:53:27 +0700

    Add feature color box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.scss";
+import ColorBox from "./components/ColorBox";
 
 function App() {
-  return <div className="app">Welcome to React Hooks!</div>;
+  return (
+    <div className="app">
+      <h2>Welcome to React Hooks!</h2>
+      <ColorBox />
+    </div>
+  );
 }
 
 export default App;
